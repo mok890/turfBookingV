1c1b92bc74c53aafa0ed6bfe7f6cefcee8a5a7ba
OmkumarD <omkumardhanorkar098@gmail.com>
2024-02-05 23:51:09 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <img
+        src="https://media.istockphoto.com/id/1403780865/photo/football-field-at-sunset.jpg?s=2048x2048&w=is&k=20&c=lrMG0RbYeKrwFGoIaCLBezqFTCq_Qxm0fTkYQNYt6W4="
+        alt="imagephoto"
+        className="resp-img"
+      ></img>
+      </div>
+    </>
+  )
+}
+
+export default App
